60f7ad23f9925af87c3facbeee77d1c668b906d1
victor okpara <victorjnr300@gmail.com>
2023-09-24 20:12:56 +0100

    new file created

---
+++ src/Components/Contact/Contact.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import NavBar from '../Navbar/Navbar'
+import Footer from '../Footer/Footer'
+
+const Contact = () => {
+  return (
+    <div>
+        <NavBar />
+        <Footer />
+    </div>
+  )
+}
+
+export default Contact
